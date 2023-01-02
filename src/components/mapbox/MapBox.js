0258f955906a52476a29b2b24e681ae7794d92e9
Andy Humes <andrew.humes@du.edu>
2023-01-02 11:30:14 -0700

    update

--- src/components/mapbox/MapBox.js
+++ src/components/mapbox/MapBox.js
@@ -1,26 +1,15 @@
 import * as React from "react";
 
-
-
 import "./mapbox.css";
-
-
-
 
 import Box from "@mui/material/Box";
 
-
-
-
-
-
-
 export default function MapBox() {
-    return (
-
-<Box>
-<h1>Map box</h1>
-</Box>
-
-    )
+   
+  return (
+    <Box>
+      <h1>Map box</h1>
+     
+    </Box>
+  );
 }
