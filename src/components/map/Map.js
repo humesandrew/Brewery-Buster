4330b4cb13508ff1699bb49dbc6aaa59ac1fd993
Andy Humes <andrew.humes@du.edu>
2023-01-15 17:54:50 -0700

    addinitialmarker

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import "leaflet/dist/leaflet.css";
+// import "leaflet/dist/leaflet.css";
 
 import { SearchContext } from "../../context/SearchContext";
 import "./map.css";
